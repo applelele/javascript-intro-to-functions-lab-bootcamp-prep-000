2886a12d90a9ef11afb656f13731a13f0030e4b7
YUKIKO IKEDA <applelele@gmail.com>
2020-02-25 02:52:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function sayHiToGrandma(string){
   if (string.toUpperCase === string) {
     return "YES INDEED!"
   } elseif (string.toLowerCase === string) {
-    return "I can't hear you."
+    return "I can't hear you!"
   }
 }
